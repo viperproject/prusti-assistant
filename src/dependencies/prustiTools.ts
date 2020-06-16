8c491b7fab68c70e8b86ed075c69917c997361ce
Julian Dunskus <julian.dunskus@gmail.com>
2020-06-16 13:55:42 +0200

    use RemoteZipExtractor for prusti tools

--- src/dependencies/prustiTools.ts
+++ src/dependencies/prustiTools.ts
@@ -1,4 +1,4 @@
-import { Platform, Dependency, FileDownloader, InstallerSequence, LocalReference, ZipExtractor } from 'vs-verification-toolbox';
+import { Platform, Dependency, RemoteZipExtractor, LocalReference } from 'vs-verification-toolbox';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
@@ -6,18 +6,11 @@ import * as config from '../config';
 
 export function prustiTools(platform: Platform, context: vscode.ExtensionContext): Dependency<config.BuildChannel> {
     const id = identifier(platform);
-    function zipInstaller(url: string): InstallerSequence {
-        return new InstallerSequence([
-            new FileDownloader(url),
-            new ZipExtractor("prusti"),
-        ]);
-    }
-
     const channel = config.BuildChannel;
     return new Dependency(
-        path.join(context.globalStoragePath, "prusti"),
-        [channel.Stable, zipInstaller(`http://viper.ethz.ch/downloads/PrustiTools${id}.zip`)],
-        [channel.Nightly, zipInstaller(`http://viper.ethz.ch/downloads/nightly/PrustiTools${id}.zip`)],
+        path.join(context.globalStoragePath, "prustiTools"),
+        [channel.Stable, new RemoteZipExtractor(`http://viper.ethz.ch/downloads/PrustiTools${id}.zip`)],
+        [channel.Nightly, new RemoteZipExtractor(`http://viper.ethz.ch/downloads/nightly/PrustiTools${id}.zip`)],
         [channel.Local, new LocalReference(config.localPrustiPath())],
     );
 }
