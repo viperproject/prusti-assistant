93af42651f73bfb8c307971b459c9c116d7b97fb
Federico Poli <federpoli@gmail.com>
2020-10-15 08:16:47 +0200

    Simplify test/index.js

--- src/test/index.ts
+++ src/test/index.ts
@@ -3,7 +3,7 @@ import * as Mocha from "mocha";
 import * as glob from "glob";
 
 // kept as-is (except for the mocha config) from `yo code` extension template
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: "tdd",
@@ -13,27 +13,25 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, "..");
 
-    return new Promise((c, e) => {
-        glob("test/*.test.js", { cwd: testsRoot }, (err, files) => {
-            if (err !== null) {
-                return e(err);
+    const files: Array<string> = await new Promise((resolve, reject) =>
+        glob(
+            'test/*.test.js',
+            {
+                cwd: testsRoot,
+            },
+            (err, files) => {
+                if (err) reject(err)
+                else resolve(files)
             }
+        )
+    )
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+    // Add files to the test suite
+    files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                e(err);
-            }
-        });
-    });
+    const failures: number = await new Promise(resolve => mocha.run(resolve))
+
+    if (failures > 0) {
+        throw new Error(`${failures} tests failed.`)
+    }
 }
