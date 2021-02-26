63ab3284bb6d7fd83e1902c43ac2039d4139b613
Federico Poli <federpoli@gmail.com>
2021-02-26 11:09:06 +0100

    Log usage of authorization token

--- src/dependencies/prustiTools.ts
+++ src/dependencies/prustiTools.ts
@@ -10,26 +10,31 @@ export function prustiTools(
 ): vvt.Dependency<config.BuildChannel> {
     const id = identifier(platform);
     const channel = config.BuildChannel;
+
+    // Token used to avoid rate limits while testing
+    const authorization_token = process.env.GITHUB_TOKEN
+    if (authorization_token) {
+        console.log("Using authorization token");
+    }
+
     const getReleaseUrl = (): Promise<string> => {
         return vvt.GitHubReleaseAsset.getLatestAssetUrl(
             "viperproject", "prusti-dev", `prusti-release-${id}.zip`,
             false,
-            // Avoid rate limit while testing
-            process.env.GITHUB_TOKEN,
+            authorization_token,
         );
     }
     const getDevUrl = (): Promise<string> => {
         return vvt.GitHubReleaseAsset.getLatestAssetUrl(
             "viperproject", "prusti-dev", `prusti-release-${id}.zip`,
             true,
-            // Avoid rate limit while testing
-            process.env.GITHUB_TOKEN,
+            authorization_token,
         );
     }
     return new vvt.Dependency(
         path.join(context.globalStoragePath, "prustiTools"),
-        [channel.LatestRelease, new vvt.GitHubZipExtractor(getReleaseUrl, "prusti")],
-        [channel.LatestDev, new vvt.GitHubZipExtractor(getDevUrl, "prusti")],
+        [channel.LatestRelease, new vvt.GitHubZipExtractor(getReleaseUrl, "prusti", authorization_token)],
+        [channel.LatestDev, new vvt.GitHubZipExtractor(getDevUrl, "prusti", authorization_token)],
         [channel.Local, new vvt.LocalReference(config.localPrustiPath())],
     );
 }
