b495091e8157a769adf2d7d3b6712b1074c1df4b
Federico Poli <federpoli@gmail.com>
2021-03-05 07:44:31 +0100

    Remove DUMP_DEBUG_INFO

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,9 +1,6 @@
 import * as path from "path";
 import * as Mocha from "mocha";
 import * as glob from "glob";
-import * as inspector from "inspector";
-import * as fs from "fs";
-import * as process from "process";
 import NYC = require("nyc");
 
 export async function run(): Promise<void> {
@@ -52,59 +49,7 @@ export async function run(): Promise<void> {
     // Add files to the test suite
     files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-    let failures = -1;
-    if (!process.env.DUMP_DEBUG_INFO) {
-        failures = await new Promise(resolve => mocha.run(resolve));
-    } else {
-        const timestamp = Math.floor(Date.now() / 1000);
-        console.log(`Start inspector (timestamp: ${timestamp})...`)
-        const session = new inspector.Session();
-        session.connect();
-        await new Promise(resolve => session.post("Profiler.enable", resolve));
-        await new Promise(resolve => session.post("Profiler.start", resolve));
-
-        // Run the tests
-        failures = await new Promise(resolve => mocha.run(resolve));
-
-        if (failures > 0) {
-            console.log("The tests failed. Dumping debug information...")
-
-            // Take a heap snapshot
-            const heapsnapshot_filename = `tests-${timestamp}.heapsnapshot`;
-            console.log(`Dump heap snapshot to '${heapsnapshot_filename}'...`)
-            const heapsnapshot_file = fs.openSync(heapsnapshot_filename, "w");
-            session.on("HeapProfiler.addHeapSnapshotChunk", (m) => {
-                fs.writeSync(heapsnapshot_file, m.params.chunk);
-            });
-            await new Promise(resolve => {
-                session.post("HeapProfiler.takeHeapSnapshot", (err) => {
-                    if (err) {
-                        console.warn(`Error while taking a snapshot of the heap: ${err}`);
-                    }
-                    resolve(null);
-                });
-            });
-            fs.closeSync(heapsnapshot_file);
-
-            // Dump CPU profile
-            const cpuprofile = await new Promise(resolve => {
-                session.post("Profiler.stop", (err, { profile }) => {
-                    if (err) {
-                        console.warn(`Error while profiling the CPU: ${err}`);
-                        resolve(null);
-                    } else {
-                        resolve(profile);
-                    }
-                });
-            });
-            const cpuprofile_filename = `tests-${timestamp}.cpuprofile`;
-            console.log(`Dump CPU profile to '${cpuprofile_filename}'...`)
-            fs.writeFileSync(cpuprofile_filename, JSON.stringify(cpuprofile));
-        }
-
-        // Clean up
-        session.disconnect();
-    }
+    const failures: number = await new Promise(resolve => mocha.run(resolve));
     await nyc.writeCoverageFile()
 
     if (failures > 0) {
