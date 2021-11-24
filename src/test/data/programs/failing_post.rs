0f764caae23badbb2a68467b4b3fedf61af9c3ef
Jonas <jonas.fiala.cardiff@gmail.com>
2021-11-24 17:01:02 +0100

    Avoid warnings in test programs

--- src/test/data/programs/failing_post.rs
+++ src/test/data/programs/failing_post.rs
@@ -5,7 +5,10 @@ fn something_true() -> bool {
     true
 }
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
 #[ensures(something_true() && false)]
 fn client(a: u32) {}
 
+#[allow(dead_code)]
 fn main() {}
