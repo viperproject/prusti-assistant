2ade500c5ec7040edbaf2c727e8001d0d77d3b65
Federico Poli <federpoli@gmail.com>
2022-02-22 11:12:30 +0100

    Update error messages

--- src/checks.ts
+++ src/checks.ts
@@ -7,10 +7,8 @@ import { PrustiLocation } from "./dependencies";
 export async function hasPrerequisites(): Promise<[boolean, string]> {
     util.log("Checking Java home...");
     if (await config.javaHome() === null) {
-        const msg = (
-            "[Prusti] Could not find Java home. Please install Java 1.8+ " +
-            "64bit or set the 'javaHome' setting, then restart the IDE."
-        );
+        const msg = "Could not find the Java home. Please install Java 11+ " +
+            "64bit or set the 'javaHome' setting, then restart the IDE.";
         return [false, msg];
     }
     util.log("Checking Rustup and Cargo...");
@@ -19,10 +17,9 @@ export async function hasPrerequisites(): Promise<[boolean, string]> {
         await util.spawn("cargo", ["--version"]);
     } catch (err) {
         util.log(`Error: ${err}`);
-        const msg = (
-            "[Prusti] Could not run Rustup. Please visit https://rustup.rs/ " +
-            "and install Rustup, then restart the IDE."
-        );
+        const msg = "Could not run Rustup. Please visit " +
+            "[https://rustup.rs/](https://rustup.rs/) and install Rustup, " +
+            "then restart the IDE.";
         return [false, msg];
     }
     util.log("Checking Java...");
@@ -33,10 +30,8 @@ export async function hasPrerequisites(): Promise<[boolean, string]> {
         await util.spawn(javaPath, ["-version"]);
     } catch (err) {
         util.log(`Error: ${err}`);
-        const msg = (
-            "[Prusti] Could not run Java. Please install Java 12+ 64bit, " +
-            "then restart the IDE."
-        );
+        const msg = "Could not run Java. Please install Java 11+ 64bit " +
+            "or set the 'javaHome' setting, then restart the IDE.";
         return [false, msg];
     }
     return [true, ""];
@@ -48,10 +43,8 @@ export async function checkPrusti(prusti: PrustiLocation): Promise<[boolean, str
         await util.spawn(prusti.z3, ["--version"]);
     } catch (err) {
         util.log(`Error: ${err}`);
-        const msg = (
-            "[Prusti] Could not run Z3. Please try updating the verifier, " +
-            "then restart the IDE."
-        );
+        const msg = "Could not run Z3. " +
+            "Please try updating the verifier, then restart the IDE.";
         return [false, msg];
     }
     util.log("Checking Prusti...");
@@ -60,8 +53,8 @@ export async function checkPrusti(prusti: PrustiLocation): Promise<[boolean, str
     } catch (err) {
         util.log("Could not run prusti-rustc");
         util.log(`Error: ${err}`);
-        const msg = "Could not run Prusti. Please try updating the verifier, " +
-            "then restart the IDE.";
+        const msg = "Could not run Prusti. " +
+            "Please try updating the verifier, then restart the IDE.";
         return [false, msg];
     }
     util.log("Checking Cargo-Prusti...");
@@ -70,8 +63,8 @@ export async function checkPrusti(prusti: PrustiLocation): Promise<[boolean, str
     } catch (err) {
         util.log("Could not run cargo-prusti");
         util.log(`Error: ${err}`);
-        const msg = "Could not run Prusti. Please try updating the verifier, " +
-            "then restart the IDE.";
+        const msg = "Could not run Prusti. " +
+            "Please try updating the verifier, then restart the IDE.";
         return [false, msg];
     }
     return [true, ""];
